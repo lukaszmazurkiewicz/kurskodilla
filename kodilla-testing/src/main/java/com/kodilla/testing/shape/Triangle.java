77562388d811ad0d40087d5f5575701bb6777be0
lukaszmazurkiewicz <manzi@o2.pl>
2018-10-14 18:37:28 +0200

    task 6.2 usuniecie out??

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Triangle.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.shape;
+
+public class Triangle implements Shape {
+
+    public void getShapeName() {
+        System.out.println("This is Triangle.");
+    }
+
+    public void getField(){
+        System.out.println("Field of the figure: ");
+    }
+}
