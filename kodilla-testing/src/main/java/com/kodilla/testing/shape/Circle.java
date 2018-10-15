ab18e9ddd660c528c9606b541dc06971322c8407
lukaszmazurkiewicz <manzi@o2.pl>
2018-10-15 17:10:55 +0200

    commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Circle.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.shape;
+
+public class Circle implements Shape {
+
+    public void getShapeName() {
+        System.out.println("This is Circle.");
+    }
+
+    public void getField(){
+        System.out.println("Field of the figure: ");
+    }
+}
