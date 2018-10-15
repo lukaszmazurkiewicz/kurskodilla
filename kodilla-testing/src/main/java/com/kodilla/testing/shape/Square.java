db07cfd2503185286cf371140df6a9ad640ac57e
lukaszmazurkiewicz <manzi@o2.pl>
2018-10-15 17:11:14 +0200

    commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Square.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.shape;
+
+public class Square implements Shape {
+
+    public void getShapeName() {
+        System.out.println("This is Square.");
+    }
+
+    public void getField(){
+        System.out.println("Field of the figure: ");
+    }
+}
