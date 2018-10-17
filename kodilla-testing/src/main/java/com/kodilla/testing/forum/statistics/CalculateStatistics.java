fb05c36a8b9f31c56d6805f7a513741dd67780df
lukaszmazurkiewicz <manzi@o2.pl>
2018-10-17 13:09:27 +0200

    task6.5

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CalculateStatistics.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.forum.statistics;
+
+import java.util.List;
+
+public class CalculateStatistics {
+    List<String> userNames;
+
+    {
+        userNames = new List<String>;
+    }
+
+    public int postCount(){}
+}
