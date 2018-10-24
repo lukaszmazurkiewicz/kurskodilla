b13aad715fa5703f33852269bc584d6e2b17adb1
lukaszmazurkiewicz <manzi@o2.pl>
2018-10-24 21:32:45 +0200

    task8.1

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -0,0 +1,11 @@
+package com.kodilla.exception.main;
+
+import java.io.FileReader;
+
+public class ExceptionModuleRunner {
+
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
+    }
+}
