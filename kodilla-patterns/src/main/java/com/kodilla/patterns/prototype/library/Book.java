c6cef566f530215cf0b4f2cab51ae6b78d764f00
lukaszmazurkiewicz <manzi@o2.pl>
2018-12-12 21:08:31 +0100

    task15.2v3

--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Book.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Book.java
@@ -1,6 +1,7 @@
 package com.kodilla.patterns.prototype.library;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public final class Book {
     final String title;
@@ -32,5 +33,20 @@ public final class Book {
                 ", author='" + author + '\'' +
                 ", publicationDate=" + publicationDate +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) &&
+                Objects.equals(author, book.author) &&
+                Objects.equals(publicationDate, book.publicationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publicationDate);
     }
 }
