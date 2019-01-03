187204223ca7eb8db3f156c782b5ad0c78bc0ec3
lukaszmazurkiewicz <manzi@o2.pl>
2019-01-03 22:17:33 +0100

    sudoku version2

--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuGameplay.java
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuGameplay.java
@@ -4,14 +4,20 @@ public class SudokuGameplay {
 
     private SudokuChecks sudokuChecks;
 
-    public void resolveSudoku() {
+    public void resolvingSudoku(SudokuBoard board) {
 
         for (int i = 0; i < SudokuBoard.NUMBER_OF_ROWS; i++) {
             for (int j = 0; j < SudokuBoard.NUMBER_OF_ROWS; j++) {
-                if (sudokuChecks.getSudoku().getSudokuBoard().get(i).getRow().get(j).getValue() == SudokuElement.EMPTY) {
-                    sudokuChecks.isOk(i,j);
-                    sudokuChecks.getSudoku().getSudokuBoard().get(i).getRow().get(j)
-                            .setValue(sudokuChecks.getSudoku().getSudokuBoard().get(i).getRow().get(j).getElements().get(0));
+                if (board.getSudokuBoard().get(i).getRow().get(j).getValue() == SudokuElement.EMPTY) {
+
+                    try {
+                        sudokuChecks.isOk(board,i,j);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    //sudokuChecks.isOk(board,i,j);
+                    board.getSudokuBoard().get(i).getRow().get(j)
+                            .setValue(board.getSudokuBoard().get(i).getRow().get(j).getElements().get(0));
                 }
             }
         }
