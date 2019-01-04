6db17d32549b4ee0c855f11176c69c7ccaba9e96
lukaszmazurkiewicz <manzi@o2.pl>
2019-01-04 19:24:56 +0100

    sudoku.v2.2

--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuChecks.java
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuChecks.java
@@ -1,31 +1,35 @@
 package com.kodilla.sudoku;
 
 public class SudokuChecks {
-    //private SudokuBoard sudokuBoard;
 
-    public void isInRow(SudokuBoard sudokuBoard, int numberOfRow) {
+    public static void isInRow(SudokuBoard sudokuBoard, int numberOfRow) {
         for (int i = 0; i < SudokuBoard.NUMBER_OF_ROWS; i++) {
             if (sudokuBoard.getSudokuBoard().get(numberOfRow).getRow().get(i).getValue() == SudokuElement.EMPTY) {
                 for (SudokuElement sudokuElement : sudokuBoard.getSudokuBoard().get(numberOfRow).getRow()) {
-                    sudokuBoard.getSudokuBoard().get(numberOfRow).getRow().get(i).getElements().remove(sudokuElement.getValue());
-                    System.out.println(sudokuElement.getElements().size());
+                    try {
+                        sudokuBoard.getSudokuBoard().get(numberOfRow).getRow().get(i).getElements().remove(sudokuElement.getValue());
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    //System.out.println(sudokuElement.getValue());
                 }
             }
         }
     }
 
-    public void isInColumn(SudokuBoard sudokuBoard, int numberOfColumn) {
+    public static void isInColumn(SudokuBoard sudokuBoard, int numberOfColumn) {
         for (int i = 0; i < SudokuBoard.NUMBER_OF_ROWS; i++) {
             if (sudokuBoard.getSudokuBoard().get(i).getRow().get(numberOfColumn).getValue() == SudokuElement.EMPTY) {
                 for (int j = 0; j < SudokuBoard.NUMBER_OF_ROWS; j++) {
                     sudokuBoard.getSudokuBoard().get(i).getRow().get(numberOfColumn).getElements()
-                            .remove(sudokuBoard.getSudokuBoard().get(j).getRow().get(numberOfColumn).getValue());
+                            .remove(sudokuBoard.getSudokuBoard().get(j).getRow().get(numberOfColumn));
+                    //System.out.println(sudokuBoard.getSudokuBoard().get(j).getRow().get(numberOfColumn).getValue());
                 }
             }
         }
     }
 
-    public void isInSquare(SudokuBoard sudokuBoard, int numberOfRow, int numberOfColumn) {
+    public static void isInSquare(SudokuBoard sudokuBoard, int numberOfRow, int numberOfColumn) {
         int r = numberOfRow - numberOfRow % 3;
         int c = numberOfColumn - numberOfColumn % 3;
 
@@ -35,7 +39,8 @@ public class SudokuChecks {
                     for(int k = r; k < r + 3; k++) {
                         for(int l = c; l < c + 3; l++) {
                             sudokuBoard.getSudokuBoard().get(i).getRow().get(j).getElements().
-                                    remove(sudokuBoard.getSudokuBoard().get(k).getRow().get(l).getValue());
+                                    remove(sudokuBoard.getSudokuBoard().get(k).getRow().get(l));
+                            //System.out.println(sudokuBoard.getSudokuBoard().get(k).getRow().get(l).getValue());
                         }
                     }
                 }
@@ -44,7 +49,7 @@ public class SudokuChecks {
         }
     }
 
-    public void isOk(SudokuBoard board, int numberOfRow, int numberOfColumn) {
+    public static void isOk(SudokuBoard board, int numberOfRow, int numberOfColumn) {
 
         isInRow(board, numberOfRow);
         isInColumn(board, numberOfColumn);
--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuGameplay.java
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuGameplay.java
@@ -2,22 +2,21 @@ package com.kodilla.sudoku;
 
 public class SudokuGameplay {
 
-    private SudokuChecks sudokuChecks;
-
     public void resolvingSudoku(SudokuBoard board) {
 
         for (int i = 0; i < SudokuBoard.NUMBER_OF_ROWS; i++) {
             for (int j = 0; j < SudokuBoard.NUMBER_OF_ROWS; j++) {
                 if (board.getSudokuBoard().get(i).getRow().get(j).getValue() == SudokuElement.EMPTY) {
 
-                    try {
+                    /*try {
                         sudokuChecks.isOk(board,i,j);
                     } catch (Exception e) {
                         System.out.println(e);
-                    }
-                    //sudokuChecks.isOk(board,i,j);
-                    board.getSudokuBoard().get(i).getRow().get(j)
-                            .setValue(board.getSudokuBoard().get(i).getRow().get(j).getElements().get(0));
+                    }*/
+                    SudokuChecks.isOk(board,i,j);
+                    //board.getSudokuBoard().get(i).getRow().get(j)
+                            //.setValue(board.getSudokuBoard().get(i).getRow().get(j).getElements().get(0));
+                    System.out.println(board.getSudokuBoard().get(i).getRow().get(j).getElements());
                 }
             }
         }
