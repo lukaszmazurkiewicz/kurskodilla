93d59117f5000d46ffb62edcb521fc502c47fb77
lukaszmazurkiewicz <manzi@o2.pl>
2019-01-06 10:37:27 +0100

    task17.3

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,12 +19,20 @@ public class InvoiceDaoTestSuite {
     @Autowired
     InvoiceDao invoiceDao;
 
+    @Autowired
+    ProductDao productDao;
+
+    @Autowired
+    ItemDao itemDao;
+
     @Test
     public void testInvoiceDaoSave() {
         //Given
         Product product1 = new Product("Pen");
         Product product2 = new Product("Pencil");
         Product product3 = new Product("Ruler");
+
+        productDao.saveAll(Arrays.asList(product1, product2, product3));
 
         Item item1 = new Item(product1, new BigDecimal(200), 20, new BigDecimal(4000));
         Item item2 = new Item(product2, new BigDecimal(100), 10, new BigDecimal(1000));
@@ -32,23 +42,21 @@ public class InvoiceDaoTestSuite {
 
         Invoice invoice1 = new Invoice("132/01/2019");
 
-        invoice1.getItemList().add(item1);
-        invoice1.getItemList().add(item2);
-        invoice1.getItemList().add(item3);
-        invoice1.getItemList().add(item4);
-        invoice1.getItemList().add(item5);
+        invoice1.setItemList(Arrays.asList(item1,item2,item3,item4,item5));
 
         //When
         invoiceDao.save(invoice1);
         String number = invoice1.getNumber();
-        int id = invoice1.getId();
+        List<Invoice> invoices = invoiceDao.findByNumber(number);
 
         //Then
-        Assert.assertEquals(1, invoiceDao.findByNumber(number));
+        Assert.assertEquals(1, invoices.size());
+        Assert.assertEquals(5, itemDao.count());
 
         //CleanUp
 
-        //invoiceDao.deleteById(id);
+        invoiceDao.delete(invoice1);
+        productDao.deleteAll(Arrays.asList(product1,product2,product3));
     }
 
 
